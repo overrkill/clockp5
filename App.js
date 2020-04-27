3b0425e8d08abceab19a5504b062da88c5349981
Abhishek Kale <50524422+overrkill@users.noreply.github.com>
2020-04-27 20:30:46 +0530

    mobile friendly design

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ let min,hr,sec;
 
 let col=[200,70,156];
 function setup(){
-  let canvas = createCanvas(400,400);
+  let canvas = createCanvas(300,300);
   canvas.parent('sketch');
  noFill();
   canvas.strokeWeight(10);
@@ -11,25 +11,22 @@ colorMode(HSB);
 console.log(col);
  }
  function draw(){
-   translate(200,200);
+   translate(150,150);
    rotate(-90);
    min=map(minute(),0,60,0,360);
    sec=map(second(),0,60,0,360);
    hr=map(hour(),0,60,0,360);
    background("#5d5d5a");
    stroke("#ffa45c");
-
-
-
-   arc(0,0,390,390,0,sec);
+   arc(0,0,290,290,0,sec);
    stroke("#ffcdab");
-   arc(0,0,300,300,0,min);
+   arc(0,0,200,200,0,min);
    stroke("#fff4e3");
-   arc(0,0,210,210,0,hr);
+   arc(0,0,110,110,0,hr);
    push();
    rotate(sec)
    stroke("#ffa45c");
-   line(0,0,150,0);
+   line(0,0,125,0);
    pop();
    push();
    rotate(min)
@@ -39,7 +36,7 @@ console.log(col);
    push();
    rotate(hr)
    stroke("#fff4e3");
-   line(0,0,50,0);
+   line(0,0,40,0);
    pop();
    noStroke();
    push();
